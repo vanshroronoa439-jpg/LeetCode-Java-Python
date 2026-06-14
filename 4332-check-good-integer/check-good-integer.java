0728e12c2a6b147c415e0f2cabd7879aa4247139
etadev <vanshroronoa439@gmail.com>
2026-06-14 09:59:37 +0530

    Time: 1 ms (95.30%) | Memory: 42.8 MB (13.15%) - LeetSync

--- 4332-check-good-integer/check-good-integer.java
+++ 4332-check-good-integer/check-good-integer.java
@@ -3,11 +3,11 @@ class Solution {
         int ds=0;
         int ss=0;
         while(n>0){
-            int d=n%10;
-            ds += d;
-            ss += d*d;
-            n /=10;
+            int d= n % 10;
+            ds += d ;
+            ss += d * d;
+            n /= 10;
         }
-        return ss-ds>=50;
+        return ss - ds >= 50 ;
     }
 }
