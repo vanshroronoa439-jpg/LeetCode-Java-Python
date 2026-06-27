03a44fd369ed0219b0cee2328f9b6851fb4c00dd
etadev <vanshroronoa439@gmail.com>
2026-06-27 12:57:15 +0530

    Time: 1 ms (25.71%) | Memory: 43.5 MB (5.22%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int len=s.length(), index;
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        for(int i=len-1;i>=0;i--){
+            index=goal.indexOf(s.substring(i,len));
+            if(index==0 && goal.substring(len-i).equals(s.substring(0,i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
