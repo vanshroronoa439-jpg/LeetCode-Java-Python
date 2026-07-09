0034b0232a07b775771f213f8c409baf74914b55
etadev <vanshroronoa439@gmail.com>
2026-07-09 06:34:17 +0530

    Time: 29 ms (5.74%) | Memory: 106.8 MB (16.65%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num)>=2){
+                return true;
+            }
+        }
+        if(map.get(nums[nums.length -1])>=2){
+            return true;
+        }
+        return false;
+    }
+}
