f75d8cf67b8c3f629d5a683845a1ff5773cf2f28
etadev <vanshroronoa439@gmail.com>
2026-07-18 15:35:54 +0530

    Time: 1 ms (76.87%) | Memory: 42.3 MB (89.44%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,16 +1,22 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
         int sumOdd=0, sumEven=0,odd=1,even=2;
-        int gcd=0;
+        int gcd=1;
         for(int i=1;i<=n;i++){
             sumOdd += odd;
             odd+=2;
             sumEven +=even;
             even+=2;
         }
-        for(int j=1;j<=sumOdd;j++){
-            if(sumOdd%j==0 && sumEven%j==0){
-                gcd=j;
+        int min=(int)Math.min(sumOdd,sumEven);
+        int max=(int)Math.max(sumOdd,sumEven);
+        while(max%min!=0){
+            gcd=max%min;
+            if(gcd<min){
+                min=gcd;
+            }
+            else{
+                max=gcd;
             }
         }
         return (gcd);
