778a6a08642f9e7f7aa06c9daa84fc57967cbd75
etadev <vanshroronoa439@gmail.com>
2026-07-24 20:24:52 +0530

    Time: 2 ms (65.92%) | Memory: 43.1 MB (71.94%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -2,16 +2,16 @@ class Solution {
     public char findTheDifference(String s, String t) {
         HashMap<Character,Integer> map=new HashMap<>();
         int len1=s.length(),len2=t.length();
-        int sum1=0, sum2=0;
+        int sum1=0;
         for(int i=0;i<len1;i++){
             char c=s.charAt(i);
-            sum1 +=c;
+            sum1 -=c;
         }
         for(int j=0;j<len2;j++){
             char ch=t.charAt(j);
-            sum2 +=ch;
+            sum1 +=ch;
         }
         
-        return (char)(sum2-sum1);
+        return (char)(sum1);
     }
 }
